c668e8a9b7a1a0dd97832b25f00f1ef8a3afa63e
MrDeath22 <daller3017@gmail.com>
2022-01-19 09:14:47 +0500

    r

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import { Nav } from "./components/nav-bar/Nav";
 import { Pages } from "./components/pages-link/Pages";
 import { Main } from "./components/main/Main";
 
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState('a');
+
+  const changeValue = (e) => {
+    e.preventDefault();
+    let val = e.target[0].value;
+    setValue((val.length < 30) ? 'yes' : 'no');
+    console.log(e.target[0].value);
+  }
+
   return (
     <div className="App">
       <Header />
       <Nav />
       <Pages />
       <Main />
+
+      <div className="container">
+        <form onSubmit={changeValue}>
+          <input name="inp"></input>
+          <button type="submit">Submit</button>
+        </form>
+        <span>{value}</span>
+      </div>
     </div>
   );
 }
